c55f469b14331b298855820186c66d8a4a040d80
Vivek Chaudhary <48665832+chaudhary-vivek@users.noreply.github.com>
2020-02-11 23:42:07 +0530

    Update XgboostRegressor.py

--- XgboostRegressor.py
+++ XgboostRegressor.py
@@ -32,7 +32,7 @@ plt.show()
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
-# Part 2: Making random forest classifier
+# Part 2: Making XGboost regressor
 
 # Importing Xgboost library
 import xgboost as xgb
